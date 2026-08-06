932461e5c2d77cd6e8e6dba6ca47779c0504631b
CristianBarrera512 <davidgg98d@gmail.com>
2026-08-06 16:56:40 -0500

    arreglos

--- OneDrive/cliente_pruebas/frontend_proyecto/src/app/pages/logis/logis.ts
+++ OneDrive/cliente_pruebas/frontend_proyecto/src/app/pages/logis/logis.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Inject } from '@angular/core';
+import { AuthService } from '../../services/auth';
 
-
-interface UsuarioSistema{
-  nombre:string;
-  correo:string;
-  password:string;
-  rol:string;
-}
 
 @Component({
   selector: 'app-logis',
@@ -22,54 +17,25 @@ export class LogisComponent {
 
 
   //constructor para usar las rutas 
-  constructor (private router:Router){}
+  constructor (private router:Router, private authService:AuthService){}
 
-  usuariosSistema:UsuarioSistema[]=[
-    {
-      nombre:'Administrador',
-      correo:'admin@sena.edu.com',
-      password:'123456',
-      rol:'Administrador'
-    },
-        {
-      nombre:'Fabian Instructor',
-      correo:'instrucotr@sena.edu.com',
-      password:'123456',
-      rol:'Instrucotr'
-    },
-    {
-      nombre:'Aprendiz',
-      correo:'Aprendiz@sena.edu.com',
-      password:'123456',
-      rol:'Aprendiz'
-    }
-  ]
 
   //Variable para alamacenar correo
   email: string='';
   // Variable para almacenar contraseña 
   password: string='';
   // CONTRASEÑA QUEMADA PARA REALIZAR LA PRUEBA
-  private readonly password_correcta: string="123456j"
-  private readonly email_correcta: string="pera000890@gmail.com"
   //metodo que sera ejecutado al precionar el boton de ingresar
   login(): void{
-      const usuario=this.usuariosSistema.find(
-        u=>u.correo===this.email
-      );
-      if(!usuario){
-        alert('el correo no existe')
-        return;
-      }
-      if (usuario.password!==this.password){
-        alert('no es la contraseña')
-      }
-      localStorage.setItem('usuarioLogeado','true');
-      localStorage.setItem('rol',usuario.rol);
-      localStorage.setItem('correo',usuario.correo);
-      localStorage.setItem('nombre',usuario.nombre)
+    const autenticado=this.authService.iniciarSesion(this.email,this.password);
 
-      alert('bienvenido' + usuario.nombre+ '\nRol' + usuario.rol)
+    if(!autenticado){
+      alert('correo o contraseña esta mal hijo de puta >:(')
+      return;
+    }
+    const usuario=this.authService.obtenerUsuario();
+      alert('bienvenido ${usuario?.nombre}\nrol:${usuario?.rol}')
+
       this.router.navigate(['/dashboard']);
     }
     
--- OneDrive/cliente_pruebas/frontend_proyecto/src/app/services/auth.ts
+++ OneDrive/cliente_pruebas/frontend_proyecto/src/app/services/auth.ts
@@ -1,4 +1,86 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { UsuarioAuth } from '../models/usuario-auth';
+import { core } from '@angular/compiler';
 
-@Service()
-export class Auth {}
+
+interface UsuarioSistema extends UsuarioAuth{
+    password:string; 
+}
+
+@Injectable({
+    providedIn:'root'
+   
+})
+export class AuthService { 
+    private readonly STORAGE_KEY='usuarioSesion';
+    
+    private usuariosSistema:UsuarioSistema[]=[
+    {
+      nombre:'Administrador',
+      correo:'admin@sena.edu.com',
+      password:'123456',
+      rol:'Administrador'
+    },
+        {
+      nombre:'Fabian Instructor',
+      correo:'instrucotr@sena.edu.com',
+      password:'123456',
+      rol:'Instrucotr'
+    },
+    {
+      nombre:'Aprendiz',
+      correo:'Aprendiz@sena.edu.com',
+      password:'123456',
+      rol:'Aprendiz'
+    }
+  ]
+
+    constructor(){}
+    // metodo para guardar la informacion del usuario
+    iniciarSesion(correo:string,password:string):boolean{
+        const usuario=this.usuariosSistema.find(
+            u=>u.correo===correo
+        );
+        if(!usuario){
+            return false;
+        }
+        if (usuario.password!==password){
+            return false;
+        }
+        const usuarioAuth:UsuarioAuth={
+            nombre:usuario.nombre,
+            correo:usuario.correo,
+            rol:usuario.rol
+        }
+        localStorage.setItem(this.STORAGE_KEY,JSON.stringify(usuarioAuth));
+        localStorage.setItem('usuarioLogeado','true');
+        return true;
+    }
+
+    cerrarSesion():void{
+        localStorage.removeItem(this.STORAGE_KEY);
+    }
+    //metodo para saber si es un usuario auteticado
+
+    estaAutenticado():boolean{
+        return localStorage.getItem(this.STORAGE_KEY)!=null;
+    }
+
+    //metodo que obtiene el usuario autenticado
+    obtenerUsuario():UsuarioAuth|null{
+        const usuario=localStorage.getItem(this.STORAGE_KEY);
+        if(!usuario){
+            return null
+        }
+        return JSON.parse(usuario)
+    }
+
+    // metodo para obtener el rol
+
+    obtenerRol():string{
+        return this.obtenerUsuario()?.rol??'';
+    }
+    obtenernombre():string{
+        return this.obtenerUsuario()?.nombre??'';
+    }
+}
